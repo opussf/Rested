72cac1620a5cd10715fac972d7644252ad0408b8
opussf <github@zz9-za.com>
2018-02-26 17:17:21 -0800

    New stubs
    <no random message>

--- test/wowStubs.lua
+++ test/wowStubs.lua
@@ -40,6 +40,10 @@ globals = {}
 accountExpansionLevel = 4   -- 0 to 5
 -- registeredPrefixes - populated by the RegisterAddonMessagePrefix( prefix )
 
+myStatistics = {
+	[60] = 42  -- 60 = deaths
+}
+
 registeredPrefixes = {}
 
 SlotListMap={ "HeadSlot","NeckSlot","ShoulderSlot","ShirtSlot","ChestSlot","WaistSlot","LegsSlot",
@@ -917,6 +921,9 @@ end
 function GetSendMailPrice()
 	-- returns: amount (in copper) to send the mail
 end
+function GetStatistic( index )
+	return myStatistics[index]
+end
 function GetTradeSkillItemLink( index )
 	if TradeSkillItems[index] then
 		return TradeSkillItems[index].ilink
@@ -960,6 +967,10 @@ function HasNewMail()
 	return true
 end
 ]]
+function GetXPExhaustion()
+	-- TODO:
+	return 3618
+end
 function InterfaceOptionsFrame_OpenToCategory()
 end
 function IsInGuild()
@@ -976,6 +987,9 @@ function IsInRaid()
 	-- 1, nill boolean return of being in raid
 	-- myParty = { ["group"] = nil, ["raid"] = nil } -- set one of these to true to reflect being in group or raid.
 	return ( myParty["raid"] and 1 or nil )
+end
+function IsResting()
+	return true
 end
 function NumTaxiNodes()
 	-- http://www.wowwiki.com/API_NumTaxiNodes
@@ -1177,6 +1191,12 @@ end
 function UnitIsDeadOrGhost( who )
 
 end
+function UnitLevel( who )
+	local unitLevels = {
+		["player"] = 60,
+	}
+	return unitLevels[who]
+end
 function UnitName( who )
 	local unitNames = {
 		["player"] = "testPlayer",
@@ -1200,6 +1220,12 @@ function UnitSex( who )
 		["player"] = 3,
 	}
 	return unitSex[who]
+end
+function UnitXP( who )
+	return 100
+end
+function UnitXPMax( who )
+	return 1000
 end
 ---------  C_WowTokenPublic
 C_WowTokenPublic = {}
