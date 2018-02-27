bad95ce5ca81952ff42ff28b7432938190d24209
opussf <github@zz9-za.com>
2018-02-26 17:20:26 -0800

    Expanded base data
    <no random message>

--- src/RestedBase.lua
+++ src/RestedBase.lua
@@ -3,9 +3,17 @@
 function Rested.SaveRestedState()
 	-- update anything based on rested state
 	-- lvlNow, xpNow, xpMax, isResting, restedPC, rested
-	--Rested.rested = GetXPExhaustion() or 0  -- XP till Exhaustion
+	Rested.rested = GetXPExhaustion() or 0  -- XP till Exhaustion
+	Rested.xpMax = UnitXPMax( "player" )
+	Rested.restedPC = ( Rested.rested > 0 and ( ( Rested.rested / Rested.xpMax ) * 100 ) or 0 )
 
 	Rested_restedState[Rested.realm][Rested.name].lvlNow = UnitLevel( "player" )
+	Rested_restedState[Rested.realm][Rested.name].xpNow = UnitXP( "player" )
+	Rested_restedState[Rested.realm][Rested.name].xpMax = Rested.xpMax
+	Rested_restedState[Rested.realm][Rested.name].isResting = IsResting()
+	Rested_restedState[Rested.realm][Rested.name].rested = Rested.rested
+	Rested_restedState[Rested.realm][Rested.name].restedPC = Rested.restedPC
+	Rested_restedState[Rested.realm][Rested.name].updated = time()
 end
 
 --[[
@@ -40,3 +48,7 @@ end
 
 Rested.InitCallback( Rested.SaveRestedState )
 Rested.EventCallback( "PLAYER_ENTERING_WORLD", Rested.SaveRestedState )
+Rested.EventCallback( "PLAYER_XP_UPDATE", Rested.SaveRestedState )
+Rested.EventCallback( "PLAYER_UPDATE_RESTING", Rested.SaveRestedState )
+Rested.EventCallback( "UPDATE_EXHAUSTION", Rested.SaveRestedState )
+Rested.EventCallback( "CHANNEL_UI_UPDATE", Rested.SaveRestedState )  -- what IS this event?
--- test/test.lua
+++ test/test.lua
@@ -18,7 +18,7 @@ test.outFileName = "testOut.xml"
 
 -- addon setup
 function test.before()
-	Rested.eventFunctions = {}
+	--Rested.eventFunctions = {}
 end
 function test.after()
 end
@@ -31,31 +31,31 @@ function test.test_InitCallback_01()
 	assertEquals( 19, Rested.bleh )
 end
 function test.test_EventCallback_01()
-	Rested.EventCallback( "PLAYER_ENTERING_WORLD", function() Rested.bleh=27; end )
+	Rested.EventCallback( "PLAYER_ENTERING_WORLD", function() Rested.bleh1=27; end )
 	Rested.PLAYER_ENTERING_WORLD( )
-	assertEquals( 27, Rested.bleh )
+	assertEquals( 27, Rested.bleh1 )
 end
 function test.test_EventCallback_noADDON_LOADED()
-	Rested.EventCallback( "ADDONLOADED", function() Rested.bleh=37; end )
+	Rested.EventCallback( "ADDON_LOADED", function() Rested.bleh2=37; end )
 	Rested.ADDON_LOADED()
-	assertEquals( 19, Rested.bleh )
+	assertIsNil( Rested.bleh2 )
 end
 function test.test_EventCallback_2Events()
-	Rested.EventCallback( "PLAYER_ENTERING_WORLD", function() Rested.bleh=42; end )
-	Rested.EventCallback( "LICK_THE_BEAR", function() Rested.bleh=43; end )
+	Rested.EventCallback( "PLAYER_ENTERING_WORLD", function() Rested.bleh3=42; end )
+	Rested.EventCallback( "LICK_THE_BEAR", function() Rested.bleh4=43; end )
 	Rested.LICK_THE_BEAR()
-	assertEquals( 43, Rested.bleh )
+	assertEquals( 43, Rested.bleh4 )
 end
 function test.test_EventCallback_2functions()
-	Rested.EventCallback( "PLAYER_ENTERING_WORLD", function() Rested.bleh=48; end )
+	Rested.EventCallback( "PLAYER_ENTERING_WORLD", function() Rested.bleh5=48; end )
 	Rested.EventCallback( "PLAYER_ENTERING_WORLD", function() Rested.yonks=49; end )
 	Rested.PLAYER_ENTERING_WORLD()
 	assertEquals( 49, Rested.yonks )
 end
 function test.test_EventCallback_EventTakesParameter()
-	Rested.EventCallback( "WITH_PARAM", function( thing ) Rested.bleh=thing; end )
+	Rested.EventCallback( "WITH_PARAM", function( thing ) Rested.bleh6=thing; end )
 	Rested.WITH_PARAM( "ThisParam" )
-	assertEquals( "ThisParam", Rested.bleh )
+	assertEquals( "ThisParam", Rested.bleh6 )
 end
 -- core data
 function test.test_CoreData_RealmName()
@@ -121,16 +121,63 @@ function test.test_BaseData_lvlNow()
 end
 function test.test_BaseData_lvlNow_PLAYER_ENTERING_WORLD()
 	Rested_restedState["testRealm"] = { ["testPlayer"] = { ["lvlNow"] = 2 } }
+	Rested.ADDON_LOADED()
 	Rested.PLAYER_ENTERING_WORLD()
 	assertEquals( 60, Rested_restedState["testRealm"]["testPlayer"]["lvlNow"] )
 end
 function test.test_BaseData_lvlNow_PLAYER_XP_UPDATE()
+	Rested_restedState["testRealm"] = { ["testPlayer"] = { ["lvlNow"] = 2 } }
+	Rested.ADDON_LOADED()
+	Rested.PLAYER_XP_UPDATE()
+	assertEquals( 60, Rested_restedState["testRealm"]["testPlayer"]["lvlNow"] )
 end
 function test.test_BaseData_lvlNow_PLAYER_UPDATE_RESTING()
+	Rested_restedState["testRealm"] = { ["testPlayer"] = { ["lvlNow"] = 2 } }
+	Rested.ADDON_LOADED()
+	Rested.PLAYER_UPDATE_RESTING()
+	assertEquals( 60, Rested_restedState["testRealm"]["testPlayer"]["lvlNow"] )
 end
 function test.test_BaseData_lvlNow_UPDATE_EXHAUSTION()
+	Rested_restedState["testRealm"] = { ["testPlayer"] = { ["lvlNow"] = 2 } }
+	Rested.ADDON_LOADED()
+	Rested.UPDATE_EXHAUSTION()
+	assertEquals( 60, Rested_restedState["testRealm"]["testPlayer"]["lvlNow"] )
 end
 function test.test_BaseData_lvlNow_CHANNEL_UI_UPDATE()
+	Rested_restedState["testRealm"] = { ["testPlayer"] = { ["lvlNow"] = 2 } }
+	Rested.ADDON_LOADED()
+	Rested.CHANNEL_UI_UPDATE()
+	assertEquals( 60, Rested_restedState["testRealm"]["testPlayer"]["lvlNow"] )
+end
+function test.test_BaseData_xpNow_PLAYER_ENTERING_WORLD()
+	Rested_restedState["testRealm"] = { ["testPlayer"] = { ["xpNow"] = 2 } }
+	Rested.ADDON_LOADED()
+	Rested.PLAYER_ENTERING_WORLD()
+	assertEquals( 100, Rested_restedState["testRealm"]["testPlayer"]["xpNow"] )
+end
+function test.test_BaseData_xpMax_PLAYER_ENTERING_WORLD()
+	Rested_restedState["testRealm"] = { ["testPlayer"] = { ["xpMax"] = 2 } }
+	Rested.ADDON_LOADED()
+	Rested.PLAYER_ENTERING_WORLD()
+	assertEquals( 1000, Rested_restedState["testRealm"]["testPlayer"]["xpMax"] )
+end
+function test.test_BaseData_isResting_PLAYER_ENTERING_WORLD()
+	Rested_restedState["testRealm"] = { ["testPlayer"] = { ["isResting"] = false } }
+	Rested.ADDON_LOADED()
+	Rested.PLAYER_ENTERING_WORLD()
+	assertTrue( Rested_restedState["testRealm"]["testPlayer"]["isResting"] )
+end
+function test.test_BaseData_rested_PLAYER_ENTERING_WORLD()
+	Rested_restedState["testRealm"] = { ["testPlayer"] = { ["rested"] = 918273987 } }
+	Rested.ADDON_LOADED()
+	Rested.PLAYER_ENTERING_WORLD()
+	assertEquals( 3618, Rested_restedState["testRealm"]["testPlayer"]["rested"] )
+end
+function test.test_BaseData_restedPC_PLAYER_ENTERING_WORLD()
+	Rested_restedState["testRealm"] = { ["testPlayer"] = { ["restedPC"] = 918273987 } }
+	Rested.ADDON_LOADED()
+	Rested.PLAYER_ENTERING_WORLD()
+	assertEquals( 361.8, Rested_restedState["testRealm"]["testPlayer"]["restedPC"] )
 end
 
 
