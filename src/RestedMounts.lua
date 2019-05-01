f326788577317729f625110934f8d4539d09ed3a
opussf <github@zz9-za.com>
2019-05-01 16:09:09 -0700

    Found an event that is fired when you mount.
    Where the spirit does not work with the hand, there is no art.

--- src/RestedMounts.lua
+++ src/RestedMounts.lua
@@ -11,38 +11,35 @@ function Rested.BuildMountSpells( )
 end
 function Rested.GetCurrentMount( ... )
 	arg1 = ...
-	if( arg1 == "player" ) then  -- only look if the event is for the player
-		if( IsMounted() ) then   -- and then, only if you are mounted
-			if( not Rested.mountSpells ) then
-				Rested.BuildMountSpells()
-			end
-			for an=1,40 do
-				aName, _, _, aType, _, _, _, _, _, aID = UnitAura( "player", an )
-				if( aName ) then
-					--print( "Aura "..an..": "..aName.." ("..(aID or "nil")..")" )
-					if( Rested.mountSpells[aID] and Rested.mountSpells[aID] == aName ) then
-						--print( aName.." is a mount." )
-						if( not Rested.currentMount ) then
-							print( "You have mounted: "..aName.." at "..date() )
-							Rested.currentMount = aID
-							Screenshot()
-							Rested_misc.mountHistory = Rested_misc.mountHistory or {}
-							Rested_misc.mountHistory[time()] = aName
-							Rested.PruneByAge( Rested_misc.mountHistory, Rested_options.mountHistoryAge )
-						end
+	if( arg1 == "MOUNT" ) then   -- only look if the event is for MOUNT
+		Rested.currentMount = nil
+		if( not Rested.mountSpells ) then
+			Rested.BuildMountSpells()
+		end
+		for an=1,40 do
+			aName, _, _, aType, _, _, _, _, _, aID = UnitAura( "player", an )
+			if( aName ) then
+				--print( "Aura "..an..": "..aName.." ("..(aID or "nil")..")" )
+				if( Rested.mountSpells[aID] and Rested.mountSpells[aID] == aName ) then
+					--print( aName.." is a mount." )
+					if( not Rested.currentMount ) then
+						print( "You have mounted: "..aName.." at "..date() )
+						Rested.currentMount = aID
+						Screenshot()
+						Rested_misc.mountHistory = Rested_misc.mountHistory or {}
+						Rested_misc.mountHistory[time()] = aName
+						Rested.PruneByAge( Rested_misc.mountHistory, Rested_options.mountHistoryAge )
 					end
-					--print( string.format( "Aura %s: %s (%s) (id=%s)", an, aName, aType, aId ) )
-				else
-					break
 				end
+				--print( string.format( "Aura %s: %s (%s) (id=%s)", an, aName, aType, aId ) )
+			else
+				break
 			end
-		else   -- not mounted, clear the currentMount
-			--print( "is NOT mounted" )
-			Rested.currentMount = nil
 		end
 	end
 end
 Rested.InitCallback( function() Rested_options.mountHistoryAge = Rested_options.mountHistoryAge or 7200; end )
 -- set the history age to 2 hours
-Rested.EventCallback( "UNIT_AURA", Rested.GetCurrentMount )
+Rested.EventCallback( "COMPANION_UPDATE", Rested.GetCurrentMount )
 Rested.EventCallback( "COMPANION_LEARNED", Rested.BuildMountSpells )
+--Rested.EventCallback( "COMPANION_UPDATE", function( ... ) cType, arg2 = ...; Rested.Print( string.format( "COMPANION_UPDATE( %s, %s )", (cType or "nil"), (arg2 or "nil") ) ); end )
