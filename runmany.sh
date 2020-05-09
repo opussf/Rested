7cdc3510e5e1b72a0f7657f46f6865f84c322fd9
opussf <github@zz9-za.com>
2020-05-09 14:13:58 -0700

    Minor update to keep this from poluting the base dir
    The heart was made to be broken.

--- runmany.sh
+++ runmany.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 ant clean
+mkdir -p target/reports
 for n in $(seq -f "%05g" 1 9999) ; do
 	#echo "Running run $n"
 	reportFile="target/reports/testOut.xml"
-	ant test > antout.txt
+	ant test > target/reports/antout.txt
 	if [ ! "$?" == "0" ]; then
-		mv antout.txt target/reports/antOut$n.txt
+		mv target/reports/antout.txt target/reports/antOut$n.txt
 		mv $reportFile target/reports/testOut$n.xml
 		ls -alt target/reports/testOut$n.xml
 	else
