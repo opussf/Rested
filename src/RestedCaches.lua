abd12ea5a8267cba6848fe480beb32d0521589c2
opussf <github@zz9-za.com>
2024-09-18 14:58:22 -0700

    This will bug
    The bogosity meter just pegged.

--- src/RestedCaches.lua
+++ src/RestedCaches.lua
@@ -4,9 +4,19 @@
 
 Rested.cacheQuests = {84736,84737,84738,84739}
 function Rested.GetCompletedCaches()
-	Rested.me.weeklyCacheCount = 0
+	local count = 0
 	for _,qnum in ipairs( Rested.cacheQuests ) do
-		Rested.me.weeklyCacheCount = Rested.me.weeklyCacheCount + (C_QuestLog.IsQuestFlaggedCompleted(qnum) and 1 or 0)
+		count = count + (C_QuestLog.IsQuestFlaggedCompleted(qnum) and 1 or 0)
+	end
+	if count ~= Rested.me.weeklyCacheCount then
+		Rested.me.weeklyCacheCount = count
+		Rested.me.weeklyCacheTS = time()
+	end
+end
+function Rested.ResetCaches( realm, name, charStruct )
+	if charStruct.weeklyCacheTS and charStruct.weeklyCacheTS <= Rested.previousWeekReset then
+		charStruct.weeklyCacheTS = nil
+		charStruct.weeklyCacheCount = nil
 	end
 end
 function Rested.CachesReport( realm, name, charStruct )
@@ -26,5 +36,14 @@ Rested.commandList["caches"] = { ["help"] = {"","Caches opened."}, ["func"] = fu
 }
 Rested.EventCallback( "QUEST_LOG_UPDATE", Rested.GetCompletedCaches )
 Rested.EventCallback( "PLAYER_ENTERING_WORLD", Rested.GetCompletedCaches )
+
+Rested.InitCallback( function()
+		C_AddOns.LoadAddOn("Blizzard_WeeklyRewards")
+		WeeklyRewardsFrame:Show()
+		_, Rested.previousWeekReset = Rested.GetWeeklyQuestResetTime()
+		Rested.ForAllChars( Rested.ResetCaches, true )
+		WeeklyRewardsFrame:Hide()
+	end
+)
 
 -- Special thanks to whomever identified the quests, and wrote an amazing macro for this.
