86741150c67b72d99542d2ac4fda44cb1dc8f527
opussf <github@zz9-za.com>
2024-11-30 15:17:32 -0800

    Farm PlotSize based on Quests, better Report
    PISCES (Feb 19 to Mar 20) Take the high road, look for the good things, carry the American Express card and a weapon. The world is yours today, as nobody else wants it. Your mortgage will be foreclosed. You will probably get run over by a bus.

--- src/RestedPandarianFarm.lua
+++ src/RestedPandarianFarm.lua
@@ -22,6 +22,38 @@ function Rested.FarmIsCrop( name )
 	return true
 end
 
+function Rested.FarmGetPlotSize()
+	local plotSizeQuests = { [2] = 30535, [4] = 30257, [8] = 30516, [12] = 30524, [16] = 30529 }
+	Rested.me.farm = Rested.me.farm or {}
+	for plotSize, qnum in Rested.SortedPairs( plotSizeQuests ) do
+		local title = C_QuestLog.GetTitleForQuestID( qnum )
+		local isComplete = C_QuestLog.IsQuestFlaggedCompleted( qnum )
+		-- print( plotSize,  C_QuestLog.IsQuestFlaggedCompleted(qnum) )
+		if title then
+			if isComplete then
+				Rested.me.farm.numPlots = plotSize
+			end
+		else
+			print( plotSize..":"..qnum.." failed to get title: Using After" )
+			C_Timer.After( 1, Rested.FarmGetPlotSize )
+			return
+		end
+	end
+	if not Rested.me.farm.numPlots then
+		Rested.me.farm = nil
+	end
+end
+function Rested.FarmGetDailyReset( )
+	-- return low, and high
+	local now = date( "*t" )
+	--print( "hour: "..now.hour )
+	if now.hour >= 2 then
+		now.hour = 2
+	end
+	Rested.FarmPrev = time(now)
+	Rested.FarmNext = Rested.FarmPrev + 86400
+end
+
 function Rested.FarmSoftFriendChanged( ... )
 	if GetSubZoneText() == "Sunsong Ranch" then
 		local unitName = UnitName("playertarget")
@@ -32,47 +64,29 @@ function Rested.FarmSoftFriendChanged( ... )
 			Rested.Command( "farm" )
 
 			Rested.me.farm = Rested.me.farm or {}
-			Rested.me.farm[unitGUID] = time()
-			Rested.me.plots = Rested.me.plots or {}
-			Rested.me.plots[unitGUID] = unitName
+			Rested.me.farm.lastHarvest = time()
 
-			local plotCount = 0
-			for k,v in pairs(Rested.me.farm) do
-				local val = tonumber(v)
-				if not val or val+64800 < time() then
-					Rested.me.farm[k] = nil
-				end
-				plotCount = plotCount + 1
-			end
-			print( (unitName or "nil").." ("..(unitGUID or "nil")..") - "..(plotCount or "nil") )
+			-- print( (unitName or "nil").." ("..(unitGUID or "nil")..") - "..(plotCount or "nil") )
 		end
 	end
 end
 
 Rested.EventCallback( "PLAYER_SOFT_FRIEND_CHANGED", Rested.FarmSoftFriendChanged )
+Rested.InitCallback( Rested.FarmGetPlotSize )
+Rested.InitCallback( Rested.FarmGetDailyReset )
 
 function Rested.FarmReport( realm, name, charStruct )
+	if not Rested.FarmPrev then Rested.FarmGetDailyReset() end
 	local rn = Rested.FormatName( realm, name )
 	if charStruct.farm then
-		local plotCount, maxTS = 0, 0
-		for k,v in pairs(charStruct.farm) do
-			local val = tonumber(v)
-			maxTS = math.max(maxTS, (val or 0))
-			if val and val + Rested_options.staleStart < time() then
-				charStruct.farm[k] = nil
-			end
-			plotCount = plotCount + 1
-		end
-		if plotCount == 0 then
-			charStruct.farm = nil
-		end
-		plotCount = math.min( plotCount, 16 )
-		table.insert( Rested.charList, { (plotCount * 150 / 16) + (1/(time()+1-maxTS)), string.format( "%0.10f :: %i :: %s", (1/(time()-maxTS)), plotCount, rn ) } )
+		-- print( realm, name, charStruct.farm, charStruct.farm.lastHarvest, charStruct.farm.numPlots )
+		table.insert( Rested.charList, { 150 - (((charStruct.farm.lastHarvest or 1) - Rested.FarmPrev) * (150/86400)),
+				string.format( "%i :: %s :: %s", (charStruct.farm.numPlots or 0), SecondsToTime( time() - (charStruct.farm.lastHarvest or 1) ), rn ) } )
 		return 1
 	end
 end
 
-Rested.reportReverseSort["Farm"] = true
+-- Rested.reportReverseSort["Farm"] = true
 Rested.dropDownMenuTable["Farm"] = "farm"
 Rested.commandList["farm"] = {["help"] = {"","Show Farm report"}, ["func"] = function()
 		Rested.reportName = "Farm"
