20feb9b389f65a50952ac2cb2787011014dd6e0d
opussf <github@zz9-za.com>
2024-12-02 13:20:37 -0800

    Move Rested.CSVFields to Rested_csv.fields to save, and drive Rested_export CSV generation.
    Go above and beyond your duty. You will benefit from it.

--- src/RestedCSV.lua
+++ src/RestedCSV.lua
@@ -1,6 +1,7 @@
 -- RestedCSV.lua
 
-Rested.CSVFields = {
+Rested_csv = {}
+Rested_csv.fields = {
 	{"Faction", "faction"},
 	{"Race", "race"},
 	{"Class", "class"},
@@ -17,7 +18,7 @@ Rested.CSVFields = {
 function Rested.MakeCSV()
 	local report = {}
 	local row = {"Realm","Name"}
-	for _, fieldStruct in ipairs( Rested.CSVFields ) do
+	for _, fieldStruct in ipairs( Rested_csv.fields ) do
 		table.insert( row, fieldStruct[1] )
 	end
 	table.insert( report, table.concat( row, "," ) )
@@ -25,19 +26,19 @@ function Rested.MakeCSV()
 	for realm, chars in Rested.SortedPairs( Rested_restedState ) do
 		for name, charStruct in Rested.SortedPairs( chars ) do
 			row = {realm, name}
-			for _, fieldStruct in ipairs( Rested.CSVFields ) do
+			for _, fieldStruct in ipairs( Rested_csv.fields ) do
 				table.insert( row, (charStruct[fieldStruct[2]] or "") )
 			end
 			table.insert( report, table.concat( row, "," ) )
 		end
 	end
-	Rested_csv = table.concat( report, "\n" ).."\n"
-	RestedCSV_EditBox:SetText( Rested_csv )
+	Rested_csv.text = table.concat( report, "\n" ).."\n"
+	RestedCSV_EditBox:SetText( Rested_csv.text )
 	RestedCSV_EditBox:HighlightText()
 	RestedCSV:Show()
 	C_Timer.After(15, function() RestedCSV:Hide(); end)
 	Rested.Print("CSV report created. Ctrl-C to copy CSV content to the clipboard.")
 end
 
-Rested.EventCallback( "PLAYER_ENTERING_WORLD", function() Rested_csv=nil; end )
+Rested.EventCallback( "PLAYER_ENTERING_WORLD", function() Rested_csv.text=nil; end )
 Rested.commandList["csv"] = {["help"] = {"","Make CSV export"}, ["func"] = Rested.MakeCSV }
