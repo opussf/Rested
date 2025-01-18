5cfbfdf5f483bec8da0c5452345ed3df379a3437
opussf <github@zz9-za.com>
2025-01-18 12:04:01 -0800

    Garrison report changes
    What type of beans are werewolf's favorite?  Human Beings.

--- src/RestedGarrisonCache.lua
+++ src/RestedGarrisonCache.lua
@@ -5,10 +5,17 @@ function Rested.GatherGarrisonResources( ... )
 	-- Rested.Print( "GatherGarrisonResources: "..type..":"..link..":"..amount )
 	if type == "currency" and strfind( link, "Garrison Resources" ) then
 		Rested.me.garrisonCache = time()
+
 	end
+end
+function Rested.GarrisonResources( ... )
+	local curInfo = C_CurrencyInfo.GetCurrencyInfo( 824 ) -- 824 = garrison resources
+	Rested.me.garrisonQuantity = curInfo.quantity
 end
 
 Rested.EventCallback( "SHOW_LOOT_TOAST", Rested.GatherGarrisonResources )
+Rested.EventCallback( "CURRENCY_DISPLAY_UPDATE", Rested.GarrisonResources )
+Rested.InitCallback( Rested.GarrisonResources )
 
 Rested.dropDownMenuTable["Garrison Cache"] = "gcache"
 Rested.commandList["gcache"] = { ["help"] = {"","Show garrison cache report."}, ["func"] = function()
@@ -23,7 +30,8 @@ function Rested.GcacheWhenAt( targetAmount, gCacheTS )
 	return ( gCacheTS + ( ( targetAmount / Rested.cacheRate ) * 3600 ) )
 end
 function Rested.GcacheReport( realm, name, charStruct )
-	if charStruct.garrisonCache then
+	if ( charStruct.garrisonCache and not charStruct.garrisonQuantity )  -- old char, no garrisonQuantity
+			or ( charStruct.garrisonCache and charStruct.garrisonQuantity < 10000 ) then  -- new char, quantity is less than max.
 		local rn = Rested.FormatName( realm, name )
 		local timeSince = time() - charStruct.garrisonCache
 
@@ -34,14 +42,16 @@ function Rested.GcacheReport( realm, name, charStruct )
 		if fullAt > time() then
 			table.insert( Rested.charList,
 					{ (resourcesInCache / Rested.cacheMax) * 150,
-						string.format( "%i : %s :: %s",
+						string.format( "%s%i : %s :: %s",
+							(charStruct.garrisonQuantity and charStruct.garrisonQuantity.." : " or ""),
 							(resourcesInCache >= Rested.cacheMin and resourcesInCache or 0),
 							SecondsToTime( fullAt - time() ),
 							rn) } )
 		else
 			table.insert( Rested.charList,
 					{ timeSince,
-						string.format( "%i : %s :: %s",
+						string.format( "%s%i : %s :: %s",
+							(charStruct.garrisonQuantity and charStruct.garrisonQuantity.." : " or ""),
 							Rested.cacheMax,
 							SecondsToTime( time() - fullAt ),
 							rn) } )
@@ -50,3 +60,33 @@ function Rested.GcacheReport( realm, name, charStruct )
 		return 1
 	end
 end
+
+--[[
+currencyID = 824
+local curInfo = C_CurrencyInfo.GetCurrencyInfo( tonumber( currencyID ) )
+		local iHaveNum = curInfo["quantity"]
+		local currencyLink = C_CurrencyInfo.GetCurrencyLink( tonumber( currencyID ), iHaveNum )
+		local gained = iHaveNum - cData.total
+		if cData.total ~= iHaveNum then
+			local progressString = string.format("%i/%i %s%s",  -- Build the progress string
+					iHaveNum, cData.needed,
+					(INEED_options.includeChange
+						and string.format("(%s%+i%s) ", ((gained > 0) and COLOR_GREEN or COLOR_RED), gained, COLOR_END)
+						or ""),
+					currencyLink)
+			_ = INEED_options.showProgress and UIErrorsFrame:AddMessage( progressString )
+			_ = INEED_options.printProgress and INEED.Print( progressString )
+			INEED_currency[currencyID]['total'] = iHaveNum
+			INEED_currency[currencyID]['updated'] = time()
+		end
+
+		]]
+
+-- Add in a garrison level like the Pandarian Farm report
+-- level : cache : Time :: rn
+-- Quests:
+-- 1: 34586: Establish Your Garrison
+-- 2: 36592:
+-- 3: 36615: My Very Own Castle
+
+-- /rested quests 34586,36615
