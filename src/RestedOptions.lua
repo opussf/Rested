4a3d4800dc3086c259d2a332037b8d0104914bb7
opussf <github@zz9-za.com>
2026-05-27 13:05:12 -0700

    Keep nagStart <= staleStart, description of duration strings, and tooltips
    Your short term goal will soon be realized.

--- src/RestedOptions.lua
+++ src/RestedOptions.lua
@@ -46,6 +46,9 @@ function Rested.OptionsPanel_DurationEditBox_Onload( self, option, text )
 end
 function Rested.OptionsPanel_DurationEditBox_OnEditFocusLost( self, option )
 	Rested_options[option] = Rested.TextToSeconds( self:GetText() )
+	if option == "nagStart" and Rested_options.nagStart > Rested_options.staleStart then
+		Rested.nagStart = Rested_options.staleStart
+	end
 	self:SetText(Rested.SecondsToText(Rested_options[option]))
 end
 
@@ -54,8 +57,10 @@ function Rested.OptionsPanel_EditBox_OnShow( self, option )
 	self:SetCursorPosition(0)
 end
 function Rested.OptionsPanel_EditBox_OnEditFocusLost( self, option )
-	print(option, self:GetText(), tonumber( self:GetText()) or 0)
 	Rested_options[option] = tonumber( self:GetText() ) or 0
+	if option == "nagIncludeOverPercentValue" and Rested_options[option] > 150 then
+		Rested_options[option] = 150
+	end
 	self:SetText( tostring( Rested_options[option] ) )
 end
 
